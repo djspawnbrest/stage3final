8ca9e9f93a28b37c6163911f0a87761bfe3e74c7
Alexander Sitnikov <djspawnbrest@gmail.com>
2019-08-07 16:43:10 +0300

    fix: connect chat to application

--- chat-ui/src/App.js
+++ chat-ui/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './css/style.css';
+import ChatController from './js/controller/ChatController';
+
+window.addEventListener('DOMContentLoaded', () => {
+  ChatController.start();
+});
 
 function App() {
   return (
     <div className="App">
-      Hello, world!
     </div>
   );
 }
